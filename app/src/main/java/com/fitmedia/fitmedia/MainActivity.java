bc9e96629f223f51748e540c7dbcf7b2a755fc45
Unknown <samuel_lfa@outlook.com>
2018-10-30 13:51:05 -0300

    Mudancas 30/10

--- app/src/main/java/com/fitmedia/fitmedia/MainActivity.java
+++ app/src/main/java/com/fitmedia/fitmedia/MainActivity.java
@@ -1,19 +1,41 @@
 package com.fitmedia.fitmedia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    private Button btnLogin;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDatabase.child("teste").setValue("teste");
+        //mDatabase.child("teste").setValue("teste");
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnRegister = (Button) findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/fitmedia/fitmedia/RegisterActivity.java
+++ app/src/main/java/com/fitmedia/fitmedia/RegisterActivity.java
@@ -1,13 +1,58 @@
 package com.fitmedia.fitmedia;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import org.joda.time.LocalDate;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private TextView data;
+    private TextView dataview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        dataview = findViewById(R.id.txt_data);
+        data = findViewById(R.id.txt_data);
+
+        data.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                final int day = c.get(Calendar.DAY_OF_MONTH);
+                final SimpleDateFormat sdfDate = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+
+                DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        LocalDate localDate = new LocalDate(year, month + 1, dayOfMonth);
+                        Date date = localDate.toDate();
+
+                        dataview.setText(sdfDate.format(date));
+
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.setTime(date);
+                        calendar.add(Calendar.HOUR, +24);
+
+                    }
+                };
+
+                DatePickerDialog dateDialog = new DatePickerDialog(RegisterActivity.this, datePickerListener, year, month, day);
+                dateDialog.show();
+            }
+        });
     }
 }
